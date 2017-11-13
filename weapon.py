16a538167e48cc96d22b081451f93e21dfbc2db2
SirFresh <wallind@mail.gvsu.edu>
2017-11-13 11:42:21 -0500

    Changed randomness to accomodate floating points in weapon.py

--- weapon.py
+++ weapon.py
@@ -2,8 +2,12 @@ from random import *
 
 class Weapon(object):
 	"""A class to store Weapon information."""
-	def __init__(self, weaponType):
+	def __init__(self, typ):
 		"""WeaponConstructor"""
-		weaponModifiers = {'HersheyKisses': 1, 'SourStraws': [0, 2]}
-		if (weaponType != 'HersheyKisses'):
-			self.Modifier = randrange(weaponModifiers[weaponType][0], weaponModifiers[weaponType][1])
+		self.weaponType = typ
+		weaponModifiers = {'HersheyKisses': 1, 'SourStraws': [1, 1.75, 2], 'ChocolateBars': [2, 2.4, 4], 'NerdBombs': [3.5, 5, 1]}
+		
+		if (typ != 'HersheyKisses'):
+			self.Modifier = uniform(weaponModifiers[typ][0], weaponModifiers[typ][1])
+		else:
+			self.Modifier = 1
