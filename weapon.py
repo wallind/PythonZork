98a7a6bd74cea9d860e2c0262eea5695fc5ce3e5
SirFresh <wallind@mail.gvsu.edu>
2017-11-13 11:51:38 -0500

    changed

--- weapon.py
+++ weapon.py
@@ -5,9 +5,13 @@ class Weapon(object):
 	def __init__(self, typ):
 		"""WeaponConstructor"""
 		self.weaponType = typ
-		weaponModifiers = {'HersheyKisses': 1, 'SourStraws': [1, 1.75, 2], 'ChocolateBars': [2, 2.4, 4], 'NerdBombs': [3.5, 5, 1]}
+		weaponModifiers = {'HersheyKisses': [1, 1, 100,000], 'SourStraws': [1, 1.75, 2], 'ChocolateBars': [2, 2.4, 4], 'NerdBombs': [3.5, 5, 1]}
 		
 		if (typ != 'HersheyKisses'):
 			self.Modifier = uniform(weaponModifiers[typ][0], weaponModifiers[typ][1])
 		else:
 			self.Modifier = 1
+		self.uses = weaponModifiers[typ][2]
+		
+	def useWeapon():
+		uses = uses - 1
